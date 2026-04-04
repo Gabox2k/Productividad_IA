ffdfba567ae4fef8667bd7ba46a12e41aba94e83
Gabriel Raidan <sforza1383@gmail,com>
2026-04-04 11:17:11 -0300

    se creo la API

---
+++ frontend/script.js
@@ -0,0 +1,21 @@
+async function verificar(){
+
+const texto = document.getElementById("texto").value
+
+const res = await fetch("http://localhost:3000/api/verificar",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({texto})
+
+})
+
+const data = await res.json()
+
+document.getElementById("resultado").innerText =
+data.choices[0].message.content
+}
